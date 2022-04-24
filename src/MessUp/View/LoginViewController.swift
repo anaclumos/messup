b2afa68316da985b00f989df48ea82ca75eb9e8c
Sunghyun Cho <mail@chosunghyun.com>
2022-04-23 18:47:09 -0700

    Change bundle identifier to sh.cho.sh.Messup

--- src/MessUp/View/LoginViewController.swift
+++ src/MessUp/View/LoginViewController.swift
@@ -14,5 +14,9 @@ class LoginViewController: UIViewController {
   @IBOutlet var LinkedInLoginButton: UIButton!
   @IBAction func LinkedInLoginButtonDidPress(_ sender: Any) {
     print("LinkedIn Login Button Pressed")
+    // alert
+    let alert = UIAlertController(title: "LinkedIn Login", message: "Please login with your LinkedIn account", preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+    self.present(alert, animated: true, completion: nil)
   }
 }
